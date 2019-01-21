b071379ea95940856c7de3573a1b60ceb75f3581
naaspati <naaspati@gmail.com>
2019-01-22 00:30:30 +0530

    minor

--- src/main/java/sam/manga/scrapper/impl/mangahere/MangaHereScrapper.java
+++ src/main/java/sam/manga/scrapper/impl/mangahere/MangaHereScrapper.java
@@ -2,17 +2,10 @@ package sam.manga.scrapper.impl.mangahere;
 
 import java.io.IOException;
 
-import org.jsoup.nodes.Element;
-
 import sam.manga.samrock.urls.MangaUrlsMeta;
 import sam.manga.scrapper.ScrappedManga;
-import sam.manga.scrapper.ScrappedPage;
-import sam.manga.scrapper.Scrapper;
 import sam.manga.scrapper.ScrapperException;
-import sam.manga.scrapper.UrlType;
-import sam.manga.scrapper.impl.mangafox.MangaFoxManga;
 import sam.manga.scrapper.impl.mangafox.MangaFoxScrapper;
-import sam.manga.scrapper.jsoup.JsoupFactory;
 
 public class MangaHereScrapper extends MangaFoxScrapper {
 	
@@ -24,5 +17,20 @@ public class MangaHereScrapper extends MangaFoxScrapper {
 	@Override
 	public ScrappedManga scrapManga(String mangaUrl) throws ScrapperException, IOException {
 		return new MangaHereManga(factory, mangaUrl);
+	}
+	
+	@Override
+	public String[] getPageImageUrl(String pageUrl) throws ScrapperException, IOException {
+		ScrapperException e ;
+		try {
+			return super.getPageImageUrl(pageUrl);
+		} catch (ScrapperException e1) {
+			e = e1;
+		}
+		try {
+			return super.getPageImageUrl(pageUrl.replace("mangahere.cc", "fanfox.net"));
+		} catch (ScrapperException e1) {}
+		
+		throw e;
 	}
 }
